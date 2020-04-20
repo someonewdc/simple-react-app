4842c3b371fba58fc23dcf96f254883b3087eef7
Ivan Klimenko <FrogonBr@gmail.com>
2020-04-20 21:21:14 +0300

    Updates

---
+++ precache-manifest.8f5dea32d38154bf72f320bb08b789e6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58148f6e14de632ada998290a38c123b",
+    "url": "/react-router-test-task/index.html"
+  },
+  {
+    "revision": "7ee09054784ad091da73",
+    "url": "/react-router-test-task/static/css/main.e153e07f.chunk.css"
+  },
+  {
+    "revision": "506ed23af93f1f5b5da0",
+    "url": "/react-router-test-task/static/js/2.bcc9b4fb.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/react-router-test-task/static/js/2.bcc9b4fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ee09054784ad091da73",
+    "url": "/react-router-test-task/static/js/main.ab18191e.chunk.js"
+  },
+  {
+    "revision": "40a9e866c10bf86dd4c2",
+    "url": "/react-router-test-task/static/js/runtime-main.be185c46.js"
+  },
+  {
+    "revision": "f647f49d242569fc5d2682c2c2265dbc",
+    "url": "/react-router-test-task/static/media/telegram.f647f49d.svg"
+  },
+  {
+    "revision": "ad9decfa7abf83f5988a0270a272eaf3",
+    "url": "/react-router-test-task/static/media/twitch.ad9decfa.svg"
+  },
+  {
+    "revision": "7e145c9eb60da95379f070be9920eca5",
+    "url": "/react-router-test-task/static/media/twitter.7e145c9e.svg"
+  },
+  {
+    "revision": "2427b10d3f2ea6605c29bab48779ddb9",
+    "url": "/react-router-test-task/static/media/vk.2427b10d.svg"
+  },
+  {
+    "revision": "4e84a27e5632393900c77917ea199ae5",
+    "url": "/react-router-test-task/static/media/web.4e84a27e.svg"
+  },
+  {
+    "revision": "2c2eb485d280d7231009064a1ffea9dc",
+    "url": "/react-router-test-task/static/media/youtube.2c2eb485.svg"
+  }
+]);
